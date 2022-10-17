23f6216cb9584a1176c28978fe62fc3059f2010e
Nikolai Naberezhnev <supovik@gmail.com>
2022-10-17 19:38:19 +0700

    добавлены тесты для 2 и 4х потоков

--- ParallelComputingLabs/Lab1/MainVM.cs
+++ ParallelComputingLabs/Lab1/MainVM.cs
@@ -11,6 +11,7 @@ namespace Lab1;
 
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
+using System.Windows.Documents;
 
 public class MainVM : ObservableObject
 {
@@ -19,6 +20,8 @@ public class MainVM : ObservableObject
     private string _firstImageSmallName = "Resources\\image1_128.jpg";
     private string _firstImageVerySmallName = "Resources\\image1_64.jpg";
     private string _firstImageMediumName = "Resources\\image1_256.jpg";
+    private string _secondImageLarge = "Resources\\image2_4096.jpg";
+    private string _secondImageSmall = "Resources\\image2_1024.jpg";
     private string CurrentDir => Directory.GetCurrentDirectory();
 
     public IRelayCommand CalculateCommand { get; set; }
@@ -32,27 +35,88 @@ public class MainVM : ObservableObject
     public MainVM()
     {
         CalculateCommand = new AsyncRelayCommand(CalculateAsync);
-        var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_firstImageName}"));
-        var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_firstImageVerySmallName}"));
+        
+       
+        //var result = new List<MyMatrix>();
+        //for (var i = 0; i < list.Count; i++)
+        //{
+        //    result.Add(MyMantrixManager.GetContrastColors(list[i], subList[i].Values[0,0]));
+        //}
+        //var a = new MyMatrix(result, M2.Height , M2.Height );
+        //ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
+    }
+
+    private async Task CalculateAll(MyMatrix M1, MyMatrix M2)
+    {
         var elementCount = M1.Height / M2.Height;
         var list = M1.GetSubMatrices(elementCount);
         var subList = M2.GetSubMatrices(1);
-        var result = new List<MyMatrix>();
-        for (var i = 0; i < list.Count; i++)
-        {
-            result.Add(MyMantrixManager.GetContrastColors(list[i], subList[i].Values[0,0]));
-        }
-        var a = new MyMatrix(result, M2.Height , M2.Height );
+        var firstStartIndex = 0;
+        var firstEndIndex = list.Count / 2;
+        var secondStartIndex = list.Count / 2;
+        var secondEndIndex = list.Count;
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var part1 = await Calculate(firstStartIndex, firstEndIndex, list, subList);
+        var part2 = await Calculate(secondStartIndex, secondEndIndex, list, subList);
+        stopwatch.Stop();
+        ElapsedMs = stopwatch.ElapsedMilliseconds;
+        part1.AddRange(part2);
+        var a = new MyMatrix(part1, M2.Height, M2.Height);
         ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
     }
 
     private async Task CalculateAsync()
     {
+        var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageLarge}"));
+        var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageSmall}"));
+
+        await CalculateAll(M1, M2);
+        //await CalculateInForThreads(M1, M2);
+    }
+
+    private async Task CalculateInForThreads(MyMatrix M1, MyMatrix M2)
+    {
+        var elementCount = M1.Height / M2.Height;
+        var list = M1.GetSubMatrices(elementCount);
+        var subList = M2.GetSubMatrices(1);
+        var subPartSize = list.Count / 4;
+        var firstStartIndex = 0;
+        var firstEndIndex = subPartSize;
+        var secondStartIndex = subPartSize;
+        var secondEndIndex = subPartSize * 2;
+        var thirdStartIndex = subPartSize * 2;
+        var thirdEndIndex = subPartSize * 3;
+        var fourthStartIndex = subPartSize * 3;
+        var fourthEndIndex = subPartSize * 4;
+
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-
-
+        var part1 = await Calculate(firstStartIndex, firstEndIndex, list, subList);
+        var part2 = await Calculate(secondStartIndex, secondEndIndex, list, subList);
+        var part3 = await Calculate(thirdStartIndex, thirdEndIndex, list, subList);
+        var part4 = await Calculate(fourthStartIndex, fourthStartIndex, list, subList);
         stopwatch.Stop();
         ElapsedMs = stopwatch.ElapsedMilliseconds;
+        part1.AddRange(part2);
+        part1.AddRange(part3);
+        part1.AddRange(part4);
+        var a = new MyMatrix(part1, M2.Height, M2.Height);
+        ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
+    }
+
+    private async Task<List<MyMatrix>> Calculate(int startIndex, int stopIndex, List<MyMatrix> m1, List<MyMatrix> m2)
+    {
+        return await Task.Run(() =>
+        {
+            var result = new List<MyMatrix>();
+
+            for (var i = startIndex; i < stopIndex; i++)
+            {
+                result.Add(MyMantrixManager.GetContrastColors(m1[i], m2[i].Values[0, 0]));
+            }
+
+            return result;
+        });
     }
 }
