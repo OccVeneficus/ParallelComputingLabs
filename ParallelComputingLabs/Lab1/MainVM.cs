2bcdb4235b60adb6a38da726719cf1e96d6bf42c
Nikolai Naberezhnev <supovik@gmail.com>
2022-10-17 21:54:12 +0700

    добавлены тесты 1 2 4 16 потоков

--- ParallelComputingLabs/Lab1/MainVM.cs
+++ ParallelComputingLabs/Lab1/MainVM.cs
@@ -16,6 +16,11 @@ using System.Windows.Documents;
 public class MainVM : ObservableObject
 {
     private long _elapsedMs;
+    private long _oneTimeSpan;
+    private long _twoTimeSpan;
+    private long _fourTimeSpan;
+    private long _sixteenTimeSpan;
+
     private string _firstImageName = "Resources\\image1_512.jpg";
     private string _firstImageSmallName = "Resources\\image1_128.jpg";
     private string _firstImageVerySmallName = "Resources\\image1_64.jpg";
@@ -24,7 +29,13 @@ public class MainVM : ObservableObject
     private string _secondImageSmall = "Resources\\image2_1024.jpg";
     private string CurrentDir => Directory.GetCurrentDirectory();
 
-    public IRelayCommand CalculateCommand { get; set; }
+    public IRelayCommand CalculateOne { get; set; }
+
+    public IRelayCommand CalculateTwo { get; set; }
+
+    public IRelayCommand CalculateFour { get; set; }
+
+    public IRelayCommand CalculateSixteen { get; set; }
 
     public long ElapsedMs
     {
@@ -32,21 +43,55 @@ public class MainVM : ObservableObject
         set => SetProperty(ref _elapsedMs, value);
     }
 
-    public MainVM()
+    public long OneTimeSpan
     {
-        CalculateCommand = new AsyncRelayCommand(CalculateAsync);
-        
-       
-        //var result = new List<MyMatrix>();
-        //for (var i = 0; i < list.Count; i++)
-        //{
-        //    result.Add(MyMantrixManager.GetContrastColors(list[i], subList[i].Values[0,0]));
-        //}
-        //var a = new MyMatrix(result, M2.Height , M2.Height );
-        //ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
+        get => _oneTimeSpan;
+        set => SetProperty(ref _oneTimeSpan, value);
     }
 
-    private async Task CalculateAll(MyMatrix M1, MyMatrix M2)
+    public long TwoTimeSpan
+    {
+        get => _twoTimeSpan;
+        set => SetProperty(ref _twoTimeSpan, value);
+    }
+
+    public long FourTimeSpan
+    {
+        get => _fourTimeSpan;
+        set => SetProperty(ref _fourTimeSpan, value);
+    }
+
+    public long SixteenTimeSpan
+    {
+        get => _sixteenTimeSpan;
+        set => SetProperty(ref _sixteenTimeSpan, value);
+    }
+
+    public MainVM()
+    {
+        CalculateOne = new AsyncRelayCommand(CalculateOneInternal);
+        CalculateTwo = new AsyncRelayCommand(CalculateTwoInternal);
+        CalculateFour = new AsyncRelayCommand(CalculateFourInternal);
+        CalculateSixteen = new AsyncRelayCommand(CalculateSixteenInternal);
+    }
+
+    private async Task CalculateInOneThread(MyMatrix M1, MyMatrix M2)
+    {
+        var elementCount = M1.Height / M2.Height;
+        var list = M1.GetSubMatrices(elementCount);
+        var subList = M2.GetSubMatrices(1);
+        var firstStartIndex = 0;
+        var firstEndIndex = list.Count;
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var part1 = await Calculate(firstStartIndex, firstEndIndex, list, subList);
+        stopwatch.Stop();
+        OneTimeSpan = stopwatch.ElapsedMilliseconds;
+        var a = new MyMatrix(part1, M2.Height, M2.Height);
+        ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
+    }
+
+    private async Task CalculateInTwoThreads(MyMatrix M1, MyMatrix M2)
     {
         var elementCount = M1.Height / M2.Height;
         var list = M1.GetSubMatrices(elementCount);
@@ -60,22 +105,46 @@ public class MainVM : ObservableObject
         var part1 = await Calculate(firstStartIndex, firstEndIndex, list, subList);
         var part2 = await Calculate(secondStartIndex, secondEndIndex, list, subList);
         stopwatch.Stop();
-        ElapsedMs = stopwatch.ElapsedMilliseconds;
+        TwoTimeSpan = stopwatch.ElapsedMilliseconds;
         part1.AddRange(part2);
         var a = new MyMatrix(part1, M2.Height, M2.Height);
         ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
     }
 
-    private async Task CalculateAsync()
+    private async Task CalculateOneInternal()
     {
         var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageLarge}"));
         var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageSmall}"));
 
-        await CalculateAll(M1, M2);
-        //await CalculateInForThreads(M1, M2);
+        await CalculateInOneThread(M1, M2);
     }
 
-    private async Task CalculateInForThreads(MyMatrix M1, MyMatrix M2)
+
+    private async Task CalculateTwoInternal()
+    {
+        var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageLarge}"));
+        var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageSmall}"));
+
+        await CalculateInTwoThreads(M1, M2);
+    }
+
+    private async Task CalculateFourInternal()
+    {
+        var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageLarge}"));
+        var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageSmall}"));
+
+        await CalculateInFourThreads(M1, M2);
+    }
+
+    private async Task CalculateSixteenInternal()
+    {
+        var M1 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageLarge}"));
+        var M2 = new MyMatrix(ImageService.LoadBitmapAsMatrix(@$"{CurrentDir}\{_secondImageSmall}"));
+
+        await CalculateInSixteenThreads(M1, M2);
+    }
+
+    private async Task CalculateInFourThreads(MyMatrix M1, MyMatrix M2)
     {
         var elementCount = M1.Height / M2.Height;
         var list = M1.GetSubMatrices(elementCount);
@@ -95,12 +164,58 @@ public class MainVM : ObservableObject
         var part1 = await Calculate(firstStartIndex, firstEndIndex, list, subList);
         var part2 = await Calculate(secondStartIndex, secondEndIndex, list, subList);
         var part3 = await Calculate(thirdStartIndex, thirdEndIndex, list, subList);
-        var part4 = await Calculate(fourthStartIndex, fourthStartIndex, list, subList);
+        var part4 = await Calculate(fourthStartIndex, fourthEndIndex, list, subList);
         stopwatch.Stop();
-        ElapsedMs = stopwatch.ElapsedMilliseconds;
+        FourTimeSpan = stopwatch.ElapsedMilliseconds;
         part1.AddRange(part2);
         part1.AddRange(part3);
         part1.AddRange(part4);
+        var a = new MyMatrix(part1, M2.Height, M2.Height);
+        ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
+    }
+
+    private async Task CalculateInSixteenThreads(MyMatrix M1, MyMatrix M2)
+    {
+        var elementCount = M1.Height / M2.Height;
+        var list = M1.GetSubMatrices(elementCount);
+        var subList = M2.GetSubMatrices(1);
+        var inexes = GetIndexes(list.Count, 16);
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var part1 = await Calculate(inexes[0], inexes[1], list, subList);
+        var part2 = await Calculate(inexes[2], inexes[3], list, subList);
+        var part3 = await Calculate(inexes[4], inexes[5], list, subList);
+        var part4 = await Calculate(inexes[6], inexes[7], list, subList);
+        var part5 = await Calculate(inexes[8], inexes[9], list, subList);
+        var part6 = await Calculate(inexes[10], inexes[11], list, subList);
+        var part7 = await Calculate(inexes[12], inexes[13], list, subList);
+        var part8 = await Calculate(inexes[14], inexes[15], list, subList);
+        var part9 = await Calculate(inexes[16], inexes[17], list, subList);
+        var part10 = await Calculate(inexes[18], inexes[19], list, subList);
+        var part11 = await Calculate(inexes[20], inexes[21], list, subList);
+        var part12 = await Calculate(inexes[22], inexes[23], list, subList);
+        var part13 = await Calculate(inexes[24], inexes[25], list, subList);
+        var part14 = await Calculate(inexes[26], inexes[27], list, subList);
+        var part15 = await Calculate(inexes[28], inexes[29], list, subList);
+        var part16 = await Calculate(inexes[30], inexes[31], list, subList);
+        stopwatch.Stop();
+        SixteenTimeSpan = stopwatch.ElapsedMilliseconds;
+        part1.AddRange(part2);
+        part1.AddRange(part3);
+        part1.AddRange(part4);
+        part1.AddRange(part5);
+        part1.AddRange(part6);
+        part1.AddRange(part7);
+        part1.AddRange(part8);
+        part1.AddRange(part9);
+        part1.AddRange(part10);
+        part1.AddRange(part11);
+        part1.AddRange(part12);
+        part1.AddRange(part13);
+        part1.AddRange(part14);
+        part1.AddRange(part15);
+        part1.AddRange(part16);
         var a = new MyMatrix(part1, M2.Height, M2.Height);
         ImageService.SaveImage(a.Values, @$"{CurrentDir}\Resources\test.jpeg");
     }
@@ -118,5 +233,21 @@ public class MainVM : ObservableObject
 
             return result;
         });
+    }
+
+    private List<int> GetIndexes(int size, int partsCount)
+    {
+        var result = new List<int>();
+        var subPartSize = size / partsCount;
+        var rangesCount = partsCount;
+        var nextIndex = 0;
+        for (var i = 0; i < rangesCount; i++)
+        {
+            result.Add(nextIndex);
+            nextIndex += subPartSize;
+            result.Add(nextIndex);
+        }
+
+        return result;
     }
 }
